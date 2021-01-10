c0e9292f76fb6bbe7b7ca0e8e5b3cf7def120020
Heather Lewis <heather.lewis@bbc.co.uk>
2021-01-10 16:13:17 +0000

    EXAMPREP3: Adds Section E: Dialog boxes

---
+++ Section1/SectionE/example2.js
@@ -0,0 +1,15 @@
+/*
+What are the differences between `alert()`, `confirm()`, and `prompt()`?
+alert();
+* creates a pop up alert box which contains the string that's passed into the alert function and an 'ok' button
+* used when you want to ensure the information gets through to the user
+* the user has to click ok to proceed
+
+confirm();
+* creates a pop up alert box which contains the string that's passed into the confirm function, an 'ok' button and a 'cancel' button
+* used if you want the user to verify something
+
+prompt();
+* creates a pop up alert box which contains the string that's passed into the alert function, an 'ok' button, a 'cancel' button and a text box for user input
+* used if you want user to enter a value before entering the page
+*/
---
+++ Section1/SectionE/screen.js
@@ -0,0 +1,8 @@
+// Screen Object
+
+/*
+The screen object provides properties/attributes of the screen on which the current window is being rendered.
+*/
+
+window.screen.height; // returns the pixel height of the screen
+window.screen.width; // returns the pixel width of the screen
