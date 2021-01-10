71cc015f41d1e9465fc7fb41d171a1cb2ed89060
Heather Lewis <heather.lewis@bbc.co.uk>
2021-01-10 16:08:13 +0000

    EXAMPREP3: Adds Section E: BOM

---
+++ Section1/SectionE/location.js
@@ -0,0 +1,23 @@
+// Location Object
+
+/*
+The location object provides read only access to information about the current location of the document.
+*/
+
+window.location.protocol;
+// returns the protocol e.g. http, https,
+
+window.location.hostname;
+// returns the name of the internet host e.g. www.bbc.co.uk
+
+window.location.pathname;
+// returns the pathname of the current page e.g. /news
+
+window.location.search;
+// returns the query string used in the search to get to that page e.g. covid
+
+// There are methods which you can use to manipulate the location object:
+
+window.location.assign();
+window.location.replace();
+window.location.reload();
---
+++ Section1/SectionE/navigator.js
@@ -0,0 +1,11 @@
+// Navigator object
+
+/*
+The navigator object provides read only information about th web browser and its capabilities
+*/
+
+window.navigator.userAgent;
+// this is a property, which enables you to identify the web browser
+
+window.navigator.appVersion;
+// this a property which enables you to find out the version of the browser (this is deprecated but may still be on the exam)
