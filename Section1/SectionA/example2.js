999e3d8a352ecfc492a1959f56a4398eb951c575
Heather Lewis <heather.lewis@bbc.co.uk>
2021-01-10 11:49:53 +0000

    Adds arithmetic operators

---
+++ Section1/SectionA/example2.js
@@ -0,0 +1,20 @@
+// Arithmetic Operators:
+
+let a = 5;
+let b = 8;
+
+console.log("+ operator", (a+b));
+console.log("- operator", (a-b));
+console.log("* operator", (a*b));
+console.log("/ operator", (a/b));
+console.log("% operator", (b%a));
+console.log("- operator", (-b));
+
+let c = -4;
+console.log("- operator", (-c));
+console.log("- operator", (+c));
+
+let d =2;
+console.log("** operator", (c**2));
+
+
