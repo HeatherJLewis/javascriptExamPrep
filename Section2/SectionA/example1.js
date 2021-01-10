f03fcf40642d5319921334590697c5253eeeccad
Heather Lewis <heather.lewis@bbc.co.uk>
2021-01-10 16:37:04 +0000

    EXAMPREP4: Adds section A Datatypes

---
+++ Section2/SectionA/example1.js
@@ -0,0 +1,31 @@
+// Declare and use variables of primitive data types.
+
+// null is the intentional absence of a value
+let a = null;
+console.log("a", typeof a); // output -> object
+
+// undefined it the unintentional absence of a value
+let b = undefined;
+console.log("b", typeof b); // output -> undefined
+
+// boolean
+let c = true;
+console.log("c", typeof c); // output -> boolean
+
+// number
+let d = 42;
+console.log("d", typeof d); // output -> number
+
+// NaN
+let e = NaN;
+console.log("e", typeof e); // output -> number
+
+// string
+let f = "forty two";
+console.log("f", typeof f); // output -> string
+
+// function
+let g = function () {};
+console.log("g", typeof g); // output -> function
+
+// Everything else is an object
